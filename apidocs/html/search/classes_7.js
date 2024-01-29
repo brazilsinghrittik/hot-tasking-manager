737d5e462f4dd696219b40fe0f349884e7086d25
Rob Savoye <rob@senecass.com>
2024-01-29 10:22:38 -0700

    Deployed 91269b9c with MkDocs version: 1.5.3

--- apidocs/html/search/classes_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['homepagestatsdto_0',['HomePageStatsDTO',['../classbackend_1_1models_1_1dtos_1_1stats__dto_1_1HomePageStatsDTO.html',1,'backend::models::dtos::stats_dto']]]
-];
